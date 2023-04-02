49e6048747a644a8f706c936b5a17f2125a0c553
TheoMour <theo.mourat02@gmail.com>
2023-04-02 22:20:56 +0300

    Prediction Pipeline and Web app

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+#Here is the place to write common functions
+# that we are going to use in the entire project.
+
 import os
 import sys
 
@@ -52,6 +55,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
